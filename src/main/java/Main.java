00215b03d7eb4cea0fe1896c8837dcfaeb4a5c0a
Alexandr <romanovskij_aj_14@mf.grsu.by>
2017-11-24 11:33:53 +0300

    Add repository, service with buisness logic and entity user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
+import service.UserService;
 
 import java.util.List;
 
@@ -12,9 +13,12 @@ public class Main {
     private PersonRepoImpl personRepoImpl;
 
     public static void main(String[] args) {
+        UserService userService = new UserService();
         ApplicationContext ctx = new ClassPathXmlApplicationContext("Beans.xml");
         Main main = ctx.getBean(Main.class);
         main.callService();
+
+        System.out.println(userService.search("jack.sparrow@grsu.local"));
     }
 
     public void callService() {
---
+++ src/main/java/configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.ldap.repository.config.EnableLdapRepositories;
+
+@Configuration
+@EnableLdapRepositories(basePackages = "local.grsu.**")
+public class AppConfig {
+}
+
